f25c601639f0df21a1e9f7e425a72b7fe5b149b2
hai <hai@LAPTOP-4DV9N6FJ>
2021-08-02 18:58:28 +0530

    6th commit
    Upload all files

---
+++ COLLECTIONS/src/ARRAYLIST.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class ARRAYLIST {
+
+	public static void main(String[] args) {
+		ArrayList<String>alist=new ArrayList<String>();
+		alist.add("raji");
+		alist.add("veni");
+		alist.add("vani");
+		System.out.println("alist");//displaying elements
+		alist.add(1,"raji");//adding "raji" at 1st position
+		System.out.println(alist);
+		
+	}
+}
+		
+
+	
+
+
